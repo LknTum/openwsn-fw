39191d48aade8c97d3e06dfbc95af7d3683dc0cb
Samuele Zoppi <samuele.zoppi@tum.de>
2016-04-22 13:09:14 +0200

    Increased load and buffer setup

--- openapps/uinject/uinject.h
+++ openapps/uinject/uinject.h
@@ -15,13 +15,13 @@
 //=========================== define ==========================================
 ///@lkn{LKN-Samu} Application periodicity
 ///@internal [LKN-uinject-settings]
-#define UINJECT_PERIOD_MS 5000  // periodicity for periodic application
+#define UINJECT_PERIOD_MS 2000  // periodicity for periodic application
 	// variance and mean for the non-periodic application
 #define BURST_SILENCE_MS 10000  // The duration of silence before a burst occurs no packet is sent under silence mode
 #define BURST_DURATION_MS 3000 // The duration of the burst mode
 
 //In total a cycle for bursty application is BURST_SILENCE_MS + BURST_DURATION_MS
-#define BURST_PERIOD_MS 700	///< packet sending period in burst mode
+#define BURST_PERIOD_MS 500	///< packet sending period in burst mode
 // Too low values such as 100 creates problems
 ///@lkn{LKN-Murat} Application type 1-Burst 2-Non-Periodic 3-Periodic
 #define APPFLAG 1
